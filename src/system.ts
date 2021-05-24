a62e84f80db0cbaa0887c3e7ab2cf4120775baab
manyyuri <ljy085@126.com>
2021-05-24 19:46:01 +0800

    fix: symbol

--- src/system.ts
+++ src/system.ts
@@ -1,7 +1,7 @@
 import { System } from '@eva/eva.js'
 
 export default class DemoSystem extends System {
-  readonly name: 'Demo';
+  readonly name = 'Demo';
 
   init() {
     console.log('demo system has been added to renderer system.')
