13c49c469ec815d68dcceb3ba5eb213047465385
fanmingfei <az8641683@163.com>
2021-12-24 19:47:29 +0800

    fix: 保持和0.0.5版本一致

--- src/type.ts
+++ src/type.ts
@@ -4,6 +4,7 @@ export default function type(type: string) {
   return function (target: any, propertyKey: string) {
     const prop = getIDEPropsPropertyObj(target, propertyKey);
     //@ts-ignore
+    prop.key = propertyKey;
     prop.type = type;
   };
 }
