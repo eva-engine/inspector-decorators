f38cef703d989f001a22f25c516cfefae620b7ca
jimizai <359743984@qq.com>
2022-03-15 15:46:37 +0800

    rm: drop old code

--- src/step.ts
+++
@@ -1,9 +0,0 @@
-import { getIDEPropsPropertyObj } from './util';
-
-export default function step(step: number) {
-  return function (target: any, propertyKey: string) {
-    const prop = getIDEPropsPropertyObj(target, propertyKey);
-    //@ts-ignore
-    prop.step = step;
-  };
-}
